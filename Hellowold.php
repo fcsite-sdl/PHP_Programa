2d35ad09125eae30bb0eb4979c0cdaa2c8cc97ac
fcsite-sdl <fcsitesw@gmail.com>
2019-10-30 14:02:10 -0300

    meu primeiro programa no GIT

---
+++ Hellowold.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Teste Francisco</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World Obrigado Jesus - perdão as coisas erradas que eu ja FIZ </p>'; ?> 
+ </body>
+</html>
